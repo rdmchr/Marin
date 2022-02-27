a579eab993066e34d6d75565fa3923860d0e2d53
Radmacher <dev@radmacher.me>
2022-02-27 19:55:15 +0100

    Added Invite and UserData type

--- apps/web/appwrite/types.ts
+++ apps/web/appwrite/types.ts
@@ -1,8 +1,22 @@
 import { Models } from "appwrite";
+import { type } from "os";
 
 export type AWList = {
     name: string;
     items: string[];
+} & Models.Document;
+
+export type AWInvite = {
+    sender: string;
+    senderName?: string;
+    addressee: string;
+    list: string;
+    listName?: string;
+} & Models.Document;
+
+export type AWLedgerUser = {
+    email: string;
+    name: string;
 } & Models.Document;
 
 export type AWAccount = {
